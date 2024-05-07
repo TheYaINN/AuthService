6c59da88e3265d3c0dff3145044168d337cf7e3e
Bengt <bengt@joachimsohn.de>
2024-05-07 22:17:03 +0200

    Initial commit

--- src/main/java/de/joachimsohn/security/JWTAuthFilter.java
+++ src/main/java/de/joachimsohn/security/JWTAuthFilter.java
@@ -16,7 +16,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-
 @Component
 @RequiredArgsConstructor
 public final class JWTAuthFilter extends OncePerRequestFilter {
